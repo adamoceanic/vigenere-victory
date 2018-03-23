4bc785cc2bc5cd019c93ec20e3aefb3e07ff70c7
Adam Jonze <ajze@users.noreply.github.com>
2018-03-23 13:39:31 +0000

    yml file c++17 updates

--- src/Lib1/VigenereDecipherer.h
+++ src/Lib1/VigenereDecipherer.h
@@ -32,13 +32,13 @@ private:
     int multiGCD(vector<int>& nums);
 
     // relative frequencies of A - Z letters in english language normalized to 0 < x < 1
-    static constexpr array<double, 26> english_frequencies_ =
+    static array<double, 26> english_frequencies_ =
             {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094,
              0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, 0.07507, 0.01929,
              0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
              0.01974, 0.00074};
 
-    static constexpr array<char, 26> eng_alphabet_ = {'A','B','C','D', 'E', 'F','G','H','I','J','K','L','M',
+    static array<char, 26> eng_alphabet_ = {'A','B','C','D', 'E', 'F','G','H','I','J','K','L','M',
                                                       'N', 'O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
 
