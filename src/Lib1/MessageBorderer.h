01e7ad9b6c31715ed6b8508c8c5518b0c94e7d38
Adam Jonze <ajze@users.noreply.github.com>
2018-03-31 00:21:42 +0100

    Update MessageBorderer.h

--- src/Lib1/MessageBorderer.h
+++ src/Lib1/MessageBorderer.h
@@ -15,7 +15,7 @@ struct MessageBorderer {
         std::cout << std::setfill(fill_char_top_) << std::setw(message_width_) << fill_char_top_ << '\n';
         for (auto& line : message) {
             std::cout << fill_char_edges_ << std::left << std::setfill(' ')
-                 << std::setw(message_width_ - 2) << line << fill_char_edges_<< '\n';
+                      << std::setw(message_width_ - 2) << line << fill_char_edges_<< '\n';
         }
         std::cout << std::setfill(fill_char_top_) << std::setw(message_width_) << fill_char_top_ << '\n';
     }
