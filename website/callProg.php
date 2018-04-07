2061645c8f68e523eed06e32e5fb3b87bf8357d3
Adam Jonze <ajze@users.noreply.github.com>
2018-04-07 20:32:32 +0100

    Update callProg.php

--- website/callProg.php
+++ website/callProg.php
@@ -1,8 +1,5 @@
 <?php
 $cipher_text = $_POST['cipher_text'];
 exec("vigapp/VigenereVictory {$cipher_text}", $ret);
-
 echo json_encode($ret);
-//echo "output: $output" . PHP_EOL;
-//echo "ret: $ret" . PHP_EOL;
-//echo var_dump($ret);
+
