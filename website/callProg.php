e02f02bc23b9ae3346a970d7ed79dba26de1113f
Adam Jonze <ajze@users.noreply.github.com>
2018-04-08 13:11:04 +0100

    website update

--- website/callProg.php
+++ website/callProg.php
@@ -2,4 +2,3 @@
 $cipher_text = $_POST['cipher_text'];
 exec("vigapp/VigenereVictory {$cipher_text}", $ret);
 echo json_encode($ret);
-
--- website/index.php
+++ website/index.php
@@ -1,25 +1,34 @@
-
-
 <!DOCTYPE html>
 <html>
 <head>
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <link rel = "stylesheet" type = "text/css" href = "css/main.css" />
   <script>
-
       $(document).ready(function() {
           $('#button').on('click', function(e) {
               e.preventDefault(); // prevent page from reloading.
-              $.ajax({
-                type: "POST",
-                url: "callProg.php",
-                data: { cipher_text: $("#input-box").val() },
-                dataType: 'json',
-                success: function(data) {
-                  var result = data[0].split("#");
-                  $("#output-box").html(result[1] + '\n' + result[2] + '\n' + result[3]);
-                }
-              });
+              var ct = $("#input-box").val();
+              if (ct.length < 800) {
+                $("#error-msg").removeClass("hide_error").addClass("show_error");
+              }
+              else {
+                $.ajax({
+                  type: "POST",
+                  url: "callProg.php",
+                  data: { cipher_text: ct},
+                  dataType: 'json',
+                  success: function(data) {
+                    var result = data[0].split("#");
+                    $("#output-box").html(result[1] + '\n' + result[2] + '\n' + result[3]);
+                  }
+                });
+              }
+          });
+
+          $('#input-box').on('click', function(e) {
+            $(this).html("");
+            $(this).removeClass("grey-text").addClass("black_text");
+            $("#error-msg").removeClass("show_error").addClass("hide_error");
           });
       });
 
@@ -30,13 +39,18 @@
   <div id="title">
     <h2>VigenereVictory</h2>
     <p><a href="https://github.com/ajze/vigenere-victory">github.com/ajze/vigenere-victory</a></p>
+    <p>This was put together quickly as a demonstration and potentially buggy. Any problems will be fixed soon.</p>
+    <div id="error-msg" class="hide_error"><p>Error: This application has not yet been proven to work consistently for strings
+    less than length 800 chars.</p></div>
   </div>
   <div id="input">
-    <textarea id="input-box" rows="10" cols="120"></textarea>
+    <textarea id="input-box" class="grey-text" rows="10" cols="120">
+      PASTE IN YOUR ALL CAPS CIPHERTEXT HERE
+    </textarea>
   </div>
   <input type="submit" id="button" value="DECRYPT"/>
   <div id="output">
-    <textarea id="output-box" rows="10" cols="120"></textarea>
+    <textarea readonly id="output-box" rows="10" cols="120"></textarea>
   </div>
 </div>
 </body>
